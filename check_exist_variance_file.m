285d65b2873e6c54fdf755e37de09b214971f2dd
Oscar Miranda-Dominguez <oscar.md.mx@gmail.com>
2021-03-24 23:19:58 -0500

    updating naming for variance files

--- check_exist_variance_file.m
+++ check_exist_variance_file.m
@@ -5,7 +5,14 @@ exist_variance_file=isfile(dtseries_var_path);
 if exist_variance_file==0
     
     [filepath,name,ext] = fileparts(dtseries_var_path);
-    alt_dtseries_var_path=[handles.alternative_path_variances filesep name ext];
+%     alt_dtseries_var_path=[handles.alternative_path_variances filesep name ext];
+    
+    opt{2}=[handles.alternative_path_variances filesep name ext];
+    opt{1}=strrep( opt{2} , '_task-rest_bold_timeseries_variance.txt' , '_task-rest_bold_desc-filtered_timeseries_variance.txt');
+    
+    ix=find(isfile(opt));
+    
+    alt_dtseries_var_path=opt{ix};
     if isfile(alt_dtseries_var_path)
         dtseries_var_path=alt_dtseries_var_path;
     end
