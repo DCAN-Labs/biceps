53c6fe44a2b0ad6de274b27d7af8b4df751409c8
Oscar Miranda-Dominguez <oscar.md.mx@gmail.com>
2022-08-09 15:00:59 -0500

    updating local path to wb at the msi

--- validate_path_wb_command.m
+++ validate_path_wb_command.m
@@ -13,10 +13,10 @@ catch
 end
 
 if local_wb_command_exist == 0
-    wb_command_on_rushmore = '/mnt/max/software/workbench/bin_linux64/wb_command';
+    wb_command_on_msi = '/home/faird/shared/code/external/utilities/workbench/1.4.2/workbench/bin_rh_linux64/wb_command';
     
-    if isfile(wb_command_on_rushmore) == 1
-        handles.paths.wb_command= wb_command_on_rushmore;
+    if isfile(wb_command_on_msi) == 1
+        handles.paths.wb_command= wb_command_on_msi;
     else        
         wb_command_vm='/opt/workbench/wb_command';
         
