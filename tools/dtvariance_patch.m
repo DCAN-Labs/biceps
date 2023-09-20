1f653728a23fbf35829f2347d58b1f177268c243
Kody DeGolier <baron063@acn22.agate.msi.umn.edu>
2023-09-20 10:45:46 -0500

    tools/dtvariance_patch.m added longitudinal functionality

--- tools/dtvariance_patch.m
+++ tools/dtvariance_patch.m
@@ -24,15 +24,15 @@ while q<=v
             output_folder=varargin{q+1};
             save_in_BIDS_folder=0;
             q = q+1;
-            
+
         case 'save_in_BIDS_folder'
             save_in_BIDS_folder=varargin{q+1};
             q = q+1;
-            
+
         case 'string_to_match'
             string_to_match=varargin{q+1};
             q = q+1;
-            
+
         otherwise
             disp(['Unknown option ',varargin{q}])
     end
@@ -61,28 +61,30 @@ n=size(list,1);
 for i=1:n
     local_path=[list{i} filesep string_to_match];
     temp=dir(local_path);
-    if size (temp,1)>0
-        dtseries_path=[temp.folder fs temp.name];
-        dtseries_path=quotes_if_space(dtseries_path);
-        
-        [filepath,name,ext]=fileparts(dtseries_path);
-        name=strrep(name,old_text,new_text);
-        
-        
-        if save_in_BIDS_folder
-        else
-            filepath=output_folder;
+    m = size (temp,1);
+    if m >0
+        for j=1:m
+
+            dtseries_path=[temp(j).folder fs temp(j).name];
+            dtseries_path=quotes_if_space(dtseries_path);
+
+            [filepath,name,ext]=fileparts(dtseries_path);
+            name=strrep(name,old_text,new_text);
+
+
+            if save_in_BIDS_folder
+            else
+                filepath=output_folder;
+            end
+
+            local_dtseries_var_path=[filepath fs name '.txt'];
+
+            local_dtseries_var_path=quotes_if_space(local_dtseries_var_path);
+
+            text_for_wb_command_cifti_stats=[handles.paths.wb_command ' -cifti-stats ' dtseries_path ' -reduce VARIANCE > ' local_dtseries_var_path];
+            display(['Calculating variance across grayordinates for dtseries: ' dtseries_path]);
+            system(text_for_wb_command_cifti_stats);
+
         end
-        
-        local_dtseries_var_path=[filepath fs name '.txt'];
-        
-        local_dtseries_var_path=quotes_if_space(local_dtseries_var_path);
-        
-        text_for_wb_command_cifti_stats=[handles.paths.wb_command ' -cifti-stats ' dtseries_path ' -reduce VARIANCE > ' local_dtseries_var_path];
-        display(['Calculating variance across grayordinates for dtseries: ' dtseries_path]);
-        system(text_for_wb_command_cifti_stats);
-        
-        
-        
     end
 end
